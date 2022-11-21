c4d34680777f49820af07f2b0c1eb6bbde105484
VladZhyrnyi <50950250+VladZhyrnyi@users.noreply.github.com>
2022-11-21 09:32:17 +0000

    Deploying to gh-pages from @ VladZhyrnyi/shop-for-gamers@05a88befb13ab49bf841ab96168bc5b4ae0ba9dc 🚀

---
+++ index.f38c3481.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector("[menu-button]"),t=document.querySelector("[menu-block]"),e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.f38c3481.js.map
